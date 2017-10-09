2afdf4e26c3e07c90497130435af090a18071489
momchil19 <mom.todorov.tues@gmail.com>
2017-10-09 22:42:05 +0300

    fix intervals

---
+++ app/controllers/intervals_controller.rb
@@ -0,0 +1,33 @@
+require 'csv'
+class IntervalsController < ApplicationController
+	protect_from_forgery except: :create
+	def create
+		file = params[:file]
+		file_path = file.path
+		result = 0
+		max = 0
+		count_rows = 0
+		CSV.foreach(file_path) do |row|
+			count_rows += 1
+		end
+		for i in 0..(count_rows - 30)
+			check = i
+			CSV.foreach(file_path) do |row|	
+				if $LINE_NUMBER > check 
+					result += row[0].to_f
+				end
+				if check - 30 == i
+					break
+				end
+				check += 1
+					
+			end
+			if check == 30
+				max == result
+			else check
+				if max < result
+					max =result
+				end
+			end
+		end	
+end
