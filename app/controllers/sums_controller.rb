b0be5ed30b1f7f2819f119a7d499b1fedfb6fbed
momchil19 <mom.todorov.tues@gmail.com>
2017-10-09 21:14:14 +0300

    second second

---
+++ app/controllers/sums_controller.rb
@@ -0,0 +1,17 @@
+require 'csv'
+class SumsController < ApplicationController
+	protect_from_forgery except: :create
+	def index
+
+	end
+	def create
+		file = params[:file]
+		file_path = file.path
+		result = 0
+		CSV.foreach(file_path) do |row|
+			result += row[0].to_f
+		end
+		result = result.ceil
+		render plain: "%.2f"%result 
+	end
+end
