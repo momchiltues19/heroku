fc2d152260fd1afb862908bbf3bf45b1dac702c5
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-22 22:55:56 +0300

    header is bad

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,4 +1,3 @@
-
 require 'csv'
 class FiltersController < ApplicationController
 	protect_from_forgery except: :create
@@ -6,7 +5,7 @@ class FiltersController < ApplicationController
 		file = params[:file]
                 file_path = file.path
                 result = 0
-                CSV.foreach(file_path) do |row|
+                CSV.foreach(file_path,headers: true) do |row|
 			if row[2].to_f%2 != 0
 				result += row[1].to_f
 			end
