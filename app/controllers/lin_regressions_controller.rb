be37f757da67b829613bc2e92bb0fda948864bfd
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-22 22:54:13 +0300

    header is good

--- app/controllers/lin_regressions_controller.rb
+++ app/controllers/lin_regressions_controller.rb
@@ -5,15 +5,16 @@ class LinRegressionsController < ApplicationController
 	def create
 		file = params[:file]
 		file_path = file.path
-		x, y = [], []
+		xs = []
+		ys = []
 		result = 0
-		count = 0
-		CSV.foreach(file_path).with_index(0) do |row,line_count|
-			x[i] = line_count
-			y[i] = row[0].to_f
-			count += 1 		
+		i = 0
+		CSV.foreach(file_path,headers: true).with_index(1) do |row,which_row|
+			xs[i] = which_row
+			ys[i] = row[0].to_f
+			i += 1 		
 		end
-		result = Regression::Linear.new x, y
+		result = Regression::Linear.new xs, ys
 		render plain: "%.6f,%.6f"% [result.slope, result.intercept] 
 	end
 end
