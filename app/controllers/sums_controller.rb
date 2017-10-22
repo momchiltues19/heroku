5b947924ba9660eaab03f398dbd72076bddce033
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-22 22:40:39 +0300

    header is not good

--- app/controllers/sums_controller.rb
+++ app/controllers/sums_controller.rb
@@ -5,11 +5,10 @@ class SumsController < ApplicationController
 		file = params[:file]
 		file_path = file.path
 		result = 0
-		CSV.foreach(file_path) do |row|
+		CSV.foreach(file_path,headers: true) do |row|
 			result += row[0].to_f
 		end
 		result = result.ceil
 		render plain: "%.2f"%result 
 	end
 end
-
